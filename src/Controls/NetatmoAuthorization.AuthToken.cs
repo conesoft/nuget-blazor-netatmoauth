170175be14f3f9c13dc35c9765b4d3118f36d6c8
David Spörri <davepermen@hotmail.com>
2024-02-26 23:11:22 +0100

    lol?

--- src/Controls/NetatmoAuthorization.AuthToken.cs
+++ src/Controls/NetatmoAuthorization.AuthToken.cs
@@ -8,7 +8,6 @@ public partial class NetatmoAuthorization
         [property: JsonPropertyName("scope")] IReadOnlyList<string> Scope,
         [property: JsonPropertyName("access_token")] string AccessToken,
         [property: JsonPropertyName("expires_in")] int ExpiresIn,
-        [property: JsonPropertyName("expire_in")] int ExpireIn,
         [property: JsonPropertyName("refresh_token")] string RefreshToken
     );
 }
